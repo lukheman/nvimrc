eb466a098e88eafb8e5e35e8c2f76c478fae794a
Akmal <pandasid05@gmail.com>
2021-08-30 21:22:25 +0800

    modified telescope display

--- lua/conf/telescope.lua
+++ lua/conf/telescope.lua
@@ -1,4 +1,6 @@
-require('telescope').setup{
+local telescope = require("telescope")
+
+telescope.setup{
   defaults = {
     vimgrep_arguments = {
       'rg',
@@ -9,39 +11,42 @@ require('telescope').setup{
       '--column',
       '--smart-case'
     },
-    prompt_prefix = "❱❱ ",
+    prompt_prefix = "   ",
+    -- prompt_prefix = "❱❱ ",
     selection_caret = "❯ ",
     entry_prefix = "  ",
     initial_mode = "insert",
     selection_strategy = "reset",
     sorting_strategy = "descending",
-    layout_strategy = "flex",
+    layout_strategy = "horizontal",
     layout_config = {
       horizontal = {
-        mirror = false,
-        height = 0.7
+        preview_width = 0.55,
+        results_width = 0.8
       },
       vertical = {
         mirror = false,
-        height = 0.7
       },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
     },
-    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+    file_sorter =  require('telescope.sorters').get_fuzzy_file,
     file_ignore_patterns = {},
-    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    generic_sorter =  require('telescope.sorters').get_generic_fuzzy_sorter,
     winblend = 0,
     border = {},
     borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
     color_devicons = true,
     use_less = true,
     set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+    file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+    grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
 
     -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
-  }
+    buffer_previewer_maker = require('telescope.previewers').buffer_previewer_maker
+  },
 }
 
 -- Highlight
