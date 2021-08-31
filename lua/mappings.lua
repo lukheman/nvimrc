647a9599f8d6c36a78ea0de5931cfdf7a60e4939
Akmal <pandasid05@gmail.com>
2021-08-31 19:27:53 +0800

    deleted useless line

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,10 +13,6 @@ local mappings = {
   { "n", "ss", "<cmd>w!<cr>", {} },
 
   -- buffer navigation
-  -- { "n", "<leader>n", ":bn<cr>", {} },
-  -- { "n", "<leader>p", ":bp<cr>", {} },
-  -- { "n", "<leader>bd", ":bp<cr>:bd! #<cr>", {} },
-  -- { "n", "<leader>bw", "<c-w><c-o>:bw<cr>", {} },
   { "n", "<leader>n", "<cmd>BufferNext<cr>", {} },
   { "n", "<leader>p", "<cmd>BufferPrevious<cr>", {} },
   { "n", "<leader>q", "<cmd>BufferDelete<cr>", {} },
@@ -48,12 +44,6 @@ local mappings = {
 
 }
 
--- run python script
-vim.cmd("autocmd FileType python map <buffer> rr :w<cr>:exec '!python3' shellescape(@%, 1)<cr>")
--- vim.cmd("autocmd FileType python imap <buffer> <C-r> <esc>:w<CR>:exec '!python3' shellescape(@%, 1)<CR>")
-
--- run lua script
-vim.cmd("autocmd FileType lua map <buffer> rr :w<cr>:exec '!lua' shellescape(@%, 1)<cr>")
 
 for _, key in ipairs(mappings) do
   keymap(key[1], key[2], key[3], key[4])
