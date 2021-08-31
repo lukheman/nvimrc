d499a454b56d59d4a9bd4784e2e0319dd19ea011
Akmal <pandasid05@gmail.com>
2021-08-31 19:24:29 +0800

    move files

--- lua/conf/vim-better-whitespace.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.config()
-  local colors = require("colors.gruvbox")
-  vim.g.better_whitespace_ctermcolor  = 'red'
-  vim.g.better_whitespace_guicolor    = colors.red
-end
-
-return M
--- lua/conf/vim-table-mode.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-function M.config()
-  vim.g.table_mode_corner='|'
-end
-
-return M
