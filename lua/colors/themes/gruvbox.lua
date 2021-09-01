92d27dc0b464a1eaed3732a6e273fa97fbe5c40a
Akmal <pandasid05@gmail.com>
2021-09-01 20:15:37 +0800

    set bright colors

--- lua/colors/themes/gruvbox.lua
+++ lua/colors/themes/gruvbox.lua
@@ -1,37 +1,4 @@
-local M = {}
-
-M.netral_colors = {
-  none = 'NONE',
-
-  -- background
-  bg0 = '#282828',
-  bg0_h = '#1d2021',
-  bg0_s = '#32302f',
-  bg1 = '#3c3836',
-  bg2 = '#504945',
-  bg3 = '#665c54',
-  bg4 = '#7c6f64',
-
-  -- foreground
-  fg0 = '#fbf1c7',
-  fg1 = '#ebdbb2',
-  fg2 = '#d5c4a1',
-  fg3 = '#bdae93',
-  fg4 = '#a89984',
-
-  -- colors
-  red = '#cc241d',
-  green = '#98971a',
-  yellow = '#d79922',
-  blue = '#458588',
-  purple = '#b16286',
-  aqua = '#689d6a',
-  gray = '#928374',
-  darkblue = '#076678',
-  orange = '#d65d0e',
-}
-
-M.bright_colors = {
+local colors = {
   none = 'NONE',
 
   -- background
@@ -62,4 +29,4 @@ M.bright_colors = {
   darkblue = '#076678'
 }
 
-return M
+return colors
