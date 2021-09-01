0f2071695567db687601ca34d5e759f259bb3575
Akmal <pandasid05@gmail.com>
2021-09-01 20:17:30 +0800

    change colors TelescopeMatching and load gruvbox theme

--- lua/colors/highlights.lua
+++ lua/colors/highlights.lua
@@ -1,9 +1,9 @@
-local c = require("colors.themes.gruvbox").bright_colors
+local c = require("colors.themes.gruvbox")
 local highlight = require("utils").highlight
 
 -- Telescope
 highlight("TelescopeSelection", {fg=c.green, gui="bold"})
-highlight("TelescopeMatching", {fg=c.gray, gui="bold"})
+highlight("TelescopeMatching", {fg=c.orange, gui="bold"})
 highlight("TelescopePromptBorder", {fg=c.gray, gui="bold"})
 highlight("TelescopeResultsBorder", {fg=c.gray, gui="bold"})
 highlight("TelescopePromptPrefix", {fg=c.gray, gui="bold"})
