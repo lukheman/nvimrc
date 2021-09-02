1cc8a6257907092a6f7a8e32618a6bb53233179d
Akmal <pandasid05@gmail.com>
2021-09-03 06:59:42 +0800

    change: colors and add fg

--- lua/colors/themes/codedark.lua
+++ lua/colors/themes/codedark.lua
@@ -9,65 +9,21 @@ local colors = {
   bg4 = '#808080',
 
   -- foreground
-  fg0 = '#d4d4d4',
+  fg0 = '#f0f0f0',
   fg1 = '#e9e9e9',
-  fg2 = '#ffffff',
+  fg2 = '#d4d4d4',
+  fg3 = '#c6c6c6',
+  fg3 = '#b8b8b8',
 
   -- colors
-  gray = '#808080',
-  -- blue = '#569cd6',
-  blue = '#398bd0',
-  darkblue = '#223e55',
-  lightblue = '#9cdcfe',
-  green = '#6a9955',
-  bluegreen = '#4ec9b0',
-  lightgreen = '#B5CEA8',
   red = '#F44747',
-  orange = '#CE9178',
-  lightred = '#D16969',
-  yelloworange = '#D7BA7D',
-  -- yellow = '#DCDCAA',
+  green = '#6a9955',
   yellow = '#D7BA7D',
-  pink = '#C586C0',
-
-  -- cursor
-  cursordarkdark = '#303030',
-  cursordark = '#51504f',
-  cursorlight = '#aeafad',
-  selection = '#264f78',
-  linenumber = '#5a5a5a',
-
-  -- tab
-  tabcurrent = '#1e1e1e',
-  tabother = '#2d2d2d',
-  taboutside = '#252526',
-
-  leftdark = '#252526',
-  leftmid = '#373737',
-  leftlight = '#3f3f46',
-
-  -- popup
-  popupfront = '#bbbbbb',
-  popupback = '#2d2d30',
-  popuphighlightblue = '#073655',
-  popuphighlightgray = '#3d3d40',
-
-  -- diff
-  diffreddark = '#4B1818',
-  diffredlight = '#6f1313',
-  diffredlightlight = '#fb0101',
-  diffgreendark = '#373d29',
-  diffgreenlight = '#4B5632',
-
-  -- search
-  searchcurrent = '#4B5632',
-  search = '#264F78',
-
-  -- split
-  splitlight = '#898989',
-  spligdark = '#444444',
-  splittumb = '#424242',
-
+  blue = '#398bd0',
+  purple = '#C586C0',
+  aqua = '#4ec9b0',
+  orange = '#CE9178',
+  gray = '#808080',
 }
 
 return colors
