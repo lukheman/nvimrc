f7514e6aa056221a76be25947b574d3ed3d0de46
Akmal <pandasid05@gmail.com>
2021-09-03 07:14:39 +0800

    change: telescope highlight

--- lua/colors/highlights.lua
+++ lua/colors/highlights.lua
@@ -2,11 +2,11 @@ local c = require("colors.themes.gruvbox")
 local highlight = require("utils").highlight
 
 -- Telescope
-highlight("TelescopeSelection", {fg=c.green, gui="bold"})
-highlight("TelescopeMatching", {fg=c.orange, gui="bold"})
-highlight("TelescopePromptBorder", {fg=c.gray, gui="bold"})
-highlight("TelescopeResultsBorder", {fg=c.gray, gui="bold"})
-highlight("TelescopePromptPrefix", {fg=c.gray, gui="bold"})
+highlight("TelescopeSelection", {fg=c.orange})
+highlight("TelescopeMatching", {fg=c.green, gui="bold"})
+highlight("TelescopePromptBorder", {fg=c.yellow, gui="bold"})
+highlight("TelescopeResultsBorder", {fg=c.yellow, gui="bold"})
+highlight("TelescopePromptPrefix", {fg=c.orange})
 
 -- Indent Blankline
 highlight("IndentBlanklineChar", { fg=c.bg2 })
