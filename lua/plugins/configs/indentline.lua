a3a7f1cc821905a8e5e1765a66f821b79dfa41d4
Akmal <pandasid05@gmail.com>
2021-09-08 19:35:57 +0800

    change configuration	modified:   lua/plugins/configs/autosave.lua change configuration	modified:   lua/plugins/configs/indentline.lua

--- lua/plugins/configs/indentline.lua
+++ lua/plugins/configs/indentline.lua
@@ -1,32 +1,34 @@
-local indentline = require("indent_blankline")
-
-indentline.setup {
-  -- '│'
-  char = '▏',
-  show_first_indent_level = false,
-  filetype_exclude = {
-    "NvimTree",
-    "Preview",
-    "__doc__",
-    "dashboard",
-    "dashpreview",
-    "fzf",
-    "help",
-    "log",
-    "man",
-    "markdown",
-    "nerdtree",
-    "peekaboo",
-    "sagahover",
-    "startify",
-    "terminal",
-    "translator",
-    "vista",
-    "packer",
-    "aerial",
-    "lspinfo",
-    "lspsagafinder",
-    "Trouble",
-  },
-  buftype_exclude = { "terminal", "man", "trouble" }
-}
+local present, indent_blankline = pcall(require, "indent_blankline")
+if present then indent_blankline.setup {
+    -- '│'
+    char = '▏',
+    show_first_indent_level = false,
+    max_indent_increase = 2,
+    show_trailing_blankline_indent = false,
+    filetype_exclude = {
+      "NvimTree",
+      "Preview",
+      "__doc__",
+      "dashboard",
+      "dashpreview",
+      "fzf",
+      "help",
+      "log",
+      "man",
+      "markdown",
+      "nerdtree",
+      "peekaboo",
+      "sagahover",
+      "startify",
+      "terminal",
+      "translator",
+      "vista",
+      "packer",
+      "aerial",
+      "lspinfo",
+      "lspsagafinder",
+      "Trouble",
+    },
+    buftype_exclude = { "terminal", "man", "trouble" }
+  }
+end
