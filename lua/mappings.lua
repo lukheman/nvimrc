2e83cbca6d09a508a722931dde7c980ce89b1ab8
Akmal <pandasid05@gmail.com>
2021-09-11 18:55:42 +0800

    change: add function in keybinds

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,5 @@
 local map = require("utils").map
+local M = {}
 
 -- leader key
 -- nnoremap <SPACE> <Nop>
@@ -12,40 +13,56 @@ map("n", "<leader>so", "<cmd>luafile %<cr>")
 map("n", "ss", "<cmd>w!<cr>")
 
 -- buffer navigation
-map("n", "<leader>n", "<cmd>BufferNext<cr>")
-map("n", "<leader>p", "<cmd>BufferPrevious<cr>")
-map("n", "<leader>q", "<cmd>BufferClose<cr>")
+function M.buffer()
+  map("n", "<leader>n", "<cmd>BufferNext<cr>")
+  map("n", "<leader>p", "<cmd>BufferPrevious<cr>")
+  map("n", "<leader>q", "<cmd>BufferClose<cr>")
+end
 
-  -- easy align
-map("v", "ga", "<Plug>(EasyAlign)", {} )
-map("n", "ga", "<Plug>(EasyAlign)", {} )
+-- easy align
+function M.autoalign()
+  map("v", "ga", "<Plug>(EasyAlign)", {} )
+  map("n", "ga", "<Plug>(EasyAlign)", {} )
+end
 
 -- clear whitespace
-map("n", "<leader>w", "<cmd>StripWhitespace<cr>")
+function M.whitespace()
+  map("n", "<leader>w", "<cmd>StripWhitespace<cr>")
+end
 
 -- floaterm
-map("n", "<leader>ft", "<cmd>FloatermNew<cr>")
-map("n", "<leader>fn", "<cmd>FloatermNext<cr>")
-map("n", "<leader>fp", "<cmd>FloatermPrev<cr>")
-map("n", "fh", "<cmd>FloatermToggle<cr>")
-map("n", "fk", "<cmd>FloatermKill<cr>")
+function M.floaterm()
+  map("n", "<leader>ft", "<cmd>FloatermNew<cr>")
+  map("n", "<leader>fn", "<cmd>FloatermNext<cr>")
+  map("n", "<leader>fp", "<cmd>FloatermPrev<cr>")
+  map("n", "fh", "<cmd>FloatermToggle<cr>")
+  map("n", "fk", "<cmd>FloatermKill<cr>")
+end
 
 -- fuzzy finder / telescope
-map("n", "<c-f>", "<cmd>lua require('telescope.builtin').find_files()<cr>")
-map("n", "<c-p>", "<cmd>lua require('telescope.builtin').buffers()<cr>")
-map("n", "<c-g>", "<cmd>lua require('telescope.builtin').git_branches()<cr>")
-map("n", "<c-h>", "<cmd>Telescope command_history<cr>")
-map("n", "<leader>c", "<cmd>Telescope commands<cr>")
+function M.telescope()
+  map("n", "<c-f>", "<cmd>lua require('telescope.builtin').find_files()<cr>")
+  map("n", "<c-p>", "<cmd>lua require('telescope.builtin').buffers()<cr>")
+  map("n", "<c-g>", "<cmd>lua require('telescope.builtin').git_branches()<cr>")
+  map("n", "<c-h>", "<cmd>Telescope command_history<cr>")
+  map("n", "<leader>c", "<cmd>Telescope commands<cr>")
+end
 
 -- nvim_tree
-map("n", "<C-n>", "<cmd>NvimTreeToggle<cr>")
-map("n", "<leader>r", "<cmd>NvimTreeRefresh<cr>")
+function M.nvimtree()
+  map("n", "<c-n>", "<cmd>nvimtreetoggle<cr>")
+  map("n", "<leader>r", "<cmd>NvimTreeRefresh<cr>")
+end
 
 -- gitsings
-map("n", "b", "<cmd>Gitsigns blame_line<cr>")
+function M.gitsings()
+  map("n", "b", "<cmd>Gitsigns blame_line<cr>")
+end
 
 -- tpope/vim-fugitive
-map("n", "<leader>gs", "<cmd>Git status<cr>")
-map("n", "<leader>ga", "<cmd>Git add %<cr>")
-map("n", "<leader>gc", "<cmd>Git commit<cr>")
-map("n", "<leader>gr", "<cmd>Git restore --staged %<cr>")
+function M.fugitive()
+  map("n", "<leader>gs", "<cmd>Git status<cr>")
+  map("n", "<leader>ga", "<cmd>Git add %<cr>")
+  map("n", "<leader>gc", "<cmd>Git commit<cr>")
+  map("n", "<leader>gr", "<cmd>Git restore --staged %<cr>")
+end
