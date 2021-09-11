67400dc02111743951d93e866d12331e57b16abe
Akmal <pandasid05@gmail.com>
2021-09-11 18:21:22 +0800

    align

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,18 +1,18 @@
 local filetypes = require("utils").filetypes
 
 -- filetypes({
-filetypes("cpp", { shiftwidth=4, tabstop=4, conceallevel=0 })
-filetypes("html", { shiftwidth=2, tabstop=2 })
-filetypes("css", { shiftwidth=2, tabstop=2 })
+filetypes("cpp",        { shiftwidth=4, tabstop=4, conceallevel=0 })
+filetypes("html",       { shiftwidth=2, tabstop=2 })
+filetypes("css",        { shiftwidth=2, tabstop=2 })
 filetypes("htmldjango", { shiftwidth=2, tabstop=2 })
 filetypes("javascript", { shiftwidth=2, tabstop=2 })
-filetypes("json", { shiftwidth=2, tabstop=2, conceallevel=0 })
-filetypes("vim", { shiftwidth=2, tabstop=2, conceallevel=0 })
-filetypes("python", { shiftwidth=4, tabstop=4})
-filetypes("lua", { shiftwidth=2, tabstop=2, softtabstop=2 })
-filetypes("php", { shiftwidth=2, tabstop=2, softtabstop=2 })
-filetypes("text", { nonumber=true, norelativenumber=true })
-filetypes("help", { nonumber=true, norelativenumber=true })
+filetypes("json",       { shiftwidth=2, tabstop=2, conceallevel=0 })
+filetypes("vim",        { shiftwidth=2, tabstop=2, conceallevel=0 })
+filetypes("python",     { shiftwidth=4, tabstop=4})
+filetypes("lua",        { shiftwidth=2, tabstop=2, softtabstop=2 })
+filetypes("php",        { shiftwidth=2, tabstop=2, softtabstop=2 })
+filetypes("text",       { nonumber=true, norelativenumber=true })
+filetypes("help",       { nonumber=true, norelativenumber=true })
 
 -- ingat posisi terakhir cursor
 vim.cmd [[ autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif ]]
