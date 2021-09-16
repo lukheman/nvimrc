458910ab6f11dd7708a2ead0f9ad9c0543e767c3
Akmal <pandasid05@gmail.com>
2021-09-16 19:20:12 +0800

    error handling

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -1,14 +1,16 @@
 local present, gitsigns = pcall(require, "gitsigns")
 
-if present then
-  gitsigns.setup {
-    signs = {
-      add          = {hl = 'GitSignsAdd'   , text = '▍'},
-      change       = {hl = 'GitSignsChange', text = '▍'},
-      changedelete = {hl = 'GitSignsChange', text = '▍'},
-      delete       = {hl = 'GitSignsDelete', text = '▍'},
-      topdelete    = {hl = 'GitSignsDelete', text = '▍'},
-    },
-    update_debounce = 500,
-  }
+if not present then
+  return
 end
+
+gitsigns.setup {
+  signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '▍'},
+    change       = {hl = 'GitSignsChange', text = '▍'},
+    changedelete = {hl = 'GitSignsChange', text = '▍'},
+    delete       = {hl = 'GitSignsDelete', text = '▍'},
+    topdelete    = {hl = 'GitSignsDelete', text = '▍'},
+  },
+  update_debounce = 500,
+}
