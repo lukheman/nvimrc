3cac362e9eb0b9d6d0a972f602b0024c19e4c380
Akmal <pandasid05@gmail.com>
2021-09-28 20:34:17 +0800

    change configuration to lua type

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -1,27 +1,40 @@
-vim.g.nvim_tree_side                  = 'left'
-vim.g.nvim_tree_width                 = 25
-vim.g.nvim_tree_ignore                = { '.git', 'node_modules', '.cache' }
-vim.g.nvim_tree_gitignore             = 1
-vim.g.nvim_tree_auto_ignore_ft        = { 'startify', 'dashboard' }
-vim.g.nvim_tree_auto_open             = 0
-vim.g.nvim_tree_auto_close            = 1
-vim.g.nvim_tree_quit_on_open          = 0
-vim.g.nvim_tree_follow                = 1
-vim.g.nvim_tree_indent_markers        = 1
-vim.g.nvim_tree_hide_dotfiles         = 1
-vim.g.nvim_tree_git_hl                = 1
-vim.g.nvim_tree_root_folder_modifier  = ':t'
-vim.g.nvim_tree_tab_open              = 1
-vim.g.nvim_tree_width_allow_resize    = 1
-vim.g.nvim_tree_disable_netrw         = 1
-vim.g.nvim_tree_hijack_netrw          = 1
-vim.g.nvim_tree_hijack_cursor         = 0
-vim.g.nvim_tree_add_trailing          = 0
-vim.g.nvim_tree_group_empty           = 0
-vim.g.nvim_tree_lsp_diagnostics       = 0
-vim.g.nvim_tree_icon_padding          = ' '
-vim.g.nvim_tree_update_cwd            = 1
-vim.g.nvim_tree_disable_window_picker = 1
+local ok, nvimtree = pcall(require, 'nvim-tree')
+
+if not ok then
+  return
+end
+
+nvimtree.setup {
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_setup = false,
+  ignore_ft_on_setup = {},
+  auto_close = true,
+  open_on_tab = false,
+  hijack_cursor = false,
+  update_cwd = false,
+  lsp_diagnostics = false,
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+  view = {
+    width = 25,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {}
+    }
+  },
+}
+
+vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_show_icons = {
   git = 1,
   folders = 1,
