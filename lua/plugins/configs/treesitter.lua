770a4889af1636d5d444a945e7d9d744310584fc
Akmal <pandasid05@gmail.com>
2021-10-03 19:20:25 +0800

    ensure_installed

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,11 +1,14 @@
+local ok, treesitter = pcall(require, "nvim-treesitter.configs")
 
-local treesitter = require("nvim-treesitter.configs")
+if not ok then
+  return
+end
 
 treesitter.setup {
-  -- ensure_installed = {
-  --   "lua",
-  --   "python"
-  -- },
+  ensure_installed = {
+    "lua",
+    "python"
+  },
   highlight = {
     enable = true,
     use_languagetree = true
