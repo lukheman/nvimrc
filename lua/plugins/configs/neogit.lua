ab3f91be31742025f19cec04fefd08f0726a365d
akmal <akmalthemalong@gmail.com>
2021-12-23 16:12:06 +0800

    added: neogit configuration

---
+++ lua/plugins/configs/neogit.lua
@@ -0,0 +1,16 @@
+local present, neogit = pcall(require, "neogit")
+
+
+if not present then
+  return
+end
+
+neogit.setup {  
+  -- customize displayed signs
+  signs = {
+    -- { CLOSED, OPENED }
+    section = { "", "" },
+    item = { "", "" },
+    hunk = { "", "" },
+  },
+}
