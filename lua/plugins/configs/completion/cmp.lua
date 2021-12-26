997e0d84483d4fc1d4a545e92377f0b0320db3bc
akmal <akmalthemalong@gmail.com>
2021-12-26 20:18:10 +0800

    change: delete useless line

--- lua/plugins/configs/completion/cmp.lua
+++ lua/plugins/configs/completion/cmp.lua
@@ -65,8 +65,6 @@ cmp.setup.cmdline('/', {
   }
 })
 
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
