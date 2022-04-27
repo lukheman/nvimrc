140437d06dc5965ce4108bf02c00b84d084dbaab
Akmal <pandasid05@gmail.com>
2022-04-27 21:10:29 +0800

    deleted: auto_close and auto_resize options

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -47,7 +47,6 @@ nvimtree.setup {
   hijack_netrw = true,
   open_on_setup = false,
   ignore_ft_on_setup = {},
-  auto_close = false,
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = false,
@@ -62,7 +61,6 @@ nvimtree.setup {
   view = {
     width = 25,
     side = 'left',
-    auto_resize = false,
   },
   renderer = {
     indent_markers = {
