ae3205bb1a526b7907da6a62ab59e5994c8b8218
Akmal <pandasid05@gmail.com>
2022-04-27 21:15:00 +0800

    deleted: telescope keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -17,21 +17,14 @@ map("n", "<leader>n", "<cmd>BufferNext<cr>")
 map("n", "<leader>p", "<cmd>BufferPrevious<cr>")
 map("n", "<leader>q", "<cmd>BufferClose<cr>")
 
-  -- easy align
+-- easy align
 map("v", "ga", "<Plug>(EasyAlign)", {} )
 map("n", "ga", "<Plug>(EasyAlign)", {} )
 
 -- clear whitespace
 map("n", "<leader>w", "<cmd>StripWhitespace<cr>")
 
--- fuzzy finder / telescope
-map("n", "<c-f>", "<cmd>lua require('telescope.builtin').find_files()<cr>")
-map("n", "<c-p>", "<cmd>lua require('telescope.builtin').buffers()<cr>")
-map("n", "<c-g>", "<cmd>lua require('telescope.builtin').git_branches()<cr>")
-map("n", "<c-h>", "<cmd>Telescope command_history<cr>")
-map("n", "<leader>c", "<cmd>Telescope commands<cr>")
-
--- nvim_tree
+-- explorer
 map("n", "<C-n>", "<cmd>NvimTreeToggle<cr>")
 map("n", "<leader>r", "<cmd>NvimTreeRefresh<cr>")
 
