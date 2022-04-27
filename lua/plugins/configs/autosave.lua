7363e7f4406763fae890abe20959efe8ed93abbd
Akmal <pandasid05@gmail.com>
2022-04-27 21:14:32 +0800

    nothing important

--- lua/plugins/configs/autosave.lua
+++ lua/plugins/configs/autosave.lua
@@ -1,17 +1,17 @@
 local present, autosave = pcall(require, "autosave")
 
-if present then
-  autosave.setup {
-    enabled = true,
-    execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
-    events = {"InsertLeave"},
-    conditions = {
-      exists = true,
-      filetype_is_not = {},
-      modifiable = true
-    },
-    write_all_buffers = false,
-    on_off_commands = true,
-    clean_command_line_interval = 2500,
-  }
-end
+if not present then return end
+
+autosave.setup {
+  enabled = true,
+  execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
+  events = {"InsertLeave"},
+  conditions = {
+    exists = true,
+    filetype_is_not = {},
+    modifiable = true
+  },
+  write_all_buffers = false,
+  on_off_commands = true,
+  clean_command_line_interval = 2500,
+}
