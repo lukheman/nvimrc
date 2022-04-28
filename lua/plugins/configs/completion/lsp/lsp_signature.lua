41c34828468efc44d9a6e392b27b15761bc823cd
Akmal <pandasid05@gmail.com>
2022-04-28 11:11:49 +0800

    add: set doc_lines and max_width options

--- lua/plugins/configs/completion/lsp/lsp_signature.lua
+++ lua/plugins/configs/completion/lsp/lsp_signature.lua
@@ -3,6 +3,7 @@ local ok, signature = pcall(require, "lsp_signature")
 if not ok then return end
 
 signature.setup {
+  doc_lines = 5,
   floating_window = true,
   floating_window_above_cur_line = false,
   fix_pos = false,
@@ -11,7 +12,7 @@ signature.setup {
   hint_scheme = "String",
   hi_parameter = "LspSignatureActiveParameter",
   max_height = 5,
-  max_width = 65,
+  max_width = 70,
   handler_opts = {
     border = 'single'
   },
