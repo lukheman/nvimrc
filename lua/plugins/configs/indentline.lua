301cafd4db09dd6f7dd06af52b35abf0638ded65
Akmal <pandasid05@gmail.com>
2022-05-14 12:33:26 +0800

    change: show first indentline

--- lua/plugins/configs/indentline.lua
+++ lua/plugins/configs/indentline.lua
@@ -5,7 +5,7 @@ end
 
 indent_blankline.setup {
   char = '▏', -- │
-  show_first_indent_level = false,
+  show_first_indent_level = true,
   max_indent_increase = 2,
   show_trailing_blankline_indent = false,
   filetype_exclude = {
