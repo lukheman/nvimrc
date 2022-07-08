62038574abf333140d223389c77e2468773e1812
Akmal <pandasid05@gmail.com>
2022-07-08 20:52:34 +0800

    formatting: stylua

--- lua/plugins/configs/Comment.lua
+++ lua/plugins/configs/Comment.lua
@@ -1,15 +1,17 @@
-local ok, comment = pcall(require, 'Comment')
+local ok, comment = pcall(require, "Comment")
 
-if not ok then return end
+if not ok then
+	return
+end
 
-comment.setup {
-  toggler = {
-    line = 'm',
-    block = 'gc'
-  },
-  mappings = {
-    basic = true,
-    extra = false,
-    extended = false
-  }
-}
+comment.setup({
+	toggler = {
+		line = "m",
+		block = "gc",
+	},
+	mappings = {
+		basic = true,
+		extra = false,
+		extended = false,
+	},
+})
