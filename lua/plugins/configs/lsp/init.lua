fb90926f186d3bf4136ee2761aab668fd889a264
Akmal <pandasid05@gmail.com>
2022-07-08 19:39:14 +0800

    reconfig and change directory

---
+++ lua/plugins/configs/lsp/init.lua
@@ -0,0 +1,108 @@
+local lspconfig = require('lspconfig')
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+
+  -- setup window diagnostic
+  vim.api.nvim_create_autocmd("CursorHold", {
+    buffer = bufnr,
+    callback = function()
+      local opts = {
+        focusable = false,
+        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+        border = 'rounded',
+        source = 'always',
+        prefix = '● ',
+        scope = 'cursor',
+      }
+      vim.diagnostic.open_float(nil, opts)
+    end
+  })
+end
+
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local servers = {
+  pyright = {
+    settings = {
+      python = {
+        analysis = {
+          autoImportCompletions = false
+        }
+      }
+    }
+  },
+  html = {
+    capabilities = capabilities
+  },
+  cssls = {
+    capabilities = capabilities
+  },
+  emmet_ls = {
+    filetypes = { "html", "css" },
+    settings = {
+      root_dir = "~",
+    }
+  },
+  tsserver = {
+    root_dir = function(fname)
+      return vim.loop.cwd()
+    end
+  }
+}
+
+for server, options in pairs(servers) do
+  options['on_attach'] = on_attach
+  lspconfig[server].setup(options)
+end
+
+
+-- UI Customization
+-- diagnostic displayed
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+
+-- diagnostic symbols
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
---
+++ lua/plugins/configs/lsp/lsp_signature.lua
@@ -0,0 +1,19 @@
+local ok, signature = pcall(require, "lsp_signature")
+
+if not ok then return end
+
+signature.setup {
+  doc_lines = 5,
+  floating_window = false,
+  floating_window_above_cur_line = true,
+  fix_pos = false,
+  hint_enable = true, -- virtual hint enable
+  hint_prefix = " ",  -- Panda for parameter
+  hint_scheme = "String",
+  hi_parameter = "LspSignatureActiveParameter",
+  max_height = 5,
+  max_width = 70,
+  handler_opts = {
+    border = 'rounded'
+  },
+}
