b43ebe30b7829c7b928de23518fa4cca3ce34465
Akmal <pandasid05@gmail.com>
2022-07-08 20:49:01 +0800

    stylua formatting

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -1,16 +1,16 @@
-local present, gitsigns = pcall(require, "gitsigns")
+local ok, gitsigns = pcall(require, "gitsigns")
 
-if not present then
-  return
+if not ok then
+	return
 end
 
-gitsigns.setup {
-  signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '▍'},
-    change       = {hl = 'GitSignsChange', text = '▍'},
-    changedelete = {hl = 'GitSignsChange', text = '▍'},
-    delete       = {hl = 'GitSignsDelete', text = '▍'},
-    topdelete    = {hl = 'GitSignsDelete', text = '▍'},
-  },
-  update_debounce = 100,
-}
+gitsigns.setup({
+	signs = {
+		add = { hl = "GitSignsAdd", text = "▍" },
+		change = { hl = "GitSignsChange", text = "▍" },
+		changedelete = { hl = "GitSignsChange", text = "▍" },
+		delete = { hl = "GitSignsDelete", text = "▍" },
+		topdelete = { hl = "GitSignsDelete", text = "▍" },
+	},
+	update_debounce = 100,
+})
