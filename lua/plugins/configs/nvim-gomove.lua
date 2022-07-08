fea1bbcf077d01d175fad61ed509266ff34d3715
Akmal <pandasid05@gmail.com>
2022-07-08 20:50:22 +0800

    stylua formatting

--- lua/plugins/configs/nvim-gomove.lua
+++ lua/plugins/configs/nvim-gomove.lua
@@ -1,10 +1,15 @@
-require("gomove").setup {
-  -- whether or not to map default key bindings, (true/false)
-  map_defaults = true,
-  -- whether or not to reindent lines moved vertically (true/false)
-  reindent = true,
-  -- whether or not to undojoin same direction moves (true/false)
-  undojoin = true,
-  -- whether to not to move past end column when moving blocks horizontally, (true/false)
-  move_past_end_col = false,
-}
+local ok, gomove = pcall(require, "gomove")
+if not ok then
+	return
+end
+
+gomove.setup({
+	-- whether or not to map default key bindings, (true/false)
+	map_defaults = true,
+	-- whether or not to reindent lines moved vertically (true/false)
+	reindent = true,
+	-- whether or not to undojoin same direction moves (true/false)
+	undojoin = true,
+	-- whether to not to move past end column when moving blocks horizontally, (true/false)
+	move_past_end_col = false,
+})
