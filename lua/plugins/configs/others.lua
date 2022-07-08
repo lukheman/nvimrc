6d9f60efba9ebacb7bc0195e4974e737f1a340f7
Akmal <pandasid05@gmail.com>
2022-07-08 20:50:55 +0800

    add: vim-translator and neogit config

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -1,13 +1,39 @@
+-- minimal configuration plugins
+
 local M = {}
 
 M.impatient = function()
-  require('impatient')
+	require("impatient")
 end
 
 M.trouble = function()
-  require('trouble').setup {
-    height = 8
-  }
+	require("trouble").setup({
+		height = 8,
+	})
+end
+
+M.vim_translator = function()
+	vim.g.translator_target_lang = "id"
+	vim.g.translator_source_lang = "auto"
+
+	vim.keymap.set("v", "<leader>t", "<cmd>Translate<cr>", { desc = "Translate" })
+end
+
+M.neogit = function()
+	local ok, neogit = pcall(require, "neogit")
+	if not ok then
+		return
+	end
+
+	neogit.setup({
+		-- customize displayed signs
+		signs = {
+			-- { CLOSED, OPENED }
+			section = { "", "" },
+			item = { "", "" },
+			hunk = { "", "" },
+		},
+	})
 end
 
 return M
