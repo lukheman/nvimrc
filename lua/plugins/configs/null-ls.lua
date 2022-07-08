cfd51099eccc9e08c1524645721fe8c30aab46bb
Akmal <pandasid05@gmail.com>
2022-07-08 10:30:13 +0800

    add: null-ls config file

---
+++ lua/plugins/configs/null-ls.lua
@@ -0,0 +1,19 @@
+local ok, nulls = pcall(require, "null-ls")
+
+if not ok then return end
+
+nulls.setup({
+  sources = {
+    -- formatting
+    -- lua
+    nulls.builtins.formatting.stylua,
+  },
+})
+
+local function format()
+  vim.lsp.buf.formatting({ async = true })
+end
+
+-- keymap
+vim.keymap.set("n", "<leader>f", format, { desc = "LSP: Formats the current buffer" })
+vim.keymap.set("v", "<leader>f", vim.lsp.buf.range_formatting, { desc = "LSP: Formats a given range" })
