93686756c36ab0b984382008831d8bec331156d5
Akmal <akmalthemalong@gmail.com>
2022-10-23 10:40:06 +0800

    delete some line

--- lua/plugins/configs/lsp/init.lua
+++ lua/plugins/configs/lsp/init.lua
@@ -31,78 +31,21 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
 
 	-- setup window diagnostic
-	vim.api.nvim_create_autocmd("CursorHold", {
-		buffer = bufnr,
-		callback = function()
-			local opts = {
-				focusable = false,
-				close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-				border = "rounded",
-				source = "always",
-				prefix = "● ",
-				scope = "cursor",
-			}
-			vim.diagnostic.open_float(nil, opts)
-		end,
-	})
+	-- vim.api.nvim_create_autocmd("CursorHold", {
+	-- 	buffer = bufnr,
+	-- 	callback = function()
+	-- 		local opts = {
+	-- 			focusable = false,
+	-- 			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+	-- 			border = "rounded",
+	-- 			source = "always",
+	-- 			prefix = "● ",
+	-- 			scope = "cursor",
+	-- 		}
+	-- 		vim.diagnostic.open_float(nil, opts)
+	-- 	end,
+	-- })
 end
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
-
-local servers = {
-	pyright = {
-		settings = {
-			python = {
-				analysis = {
-					autoImportCompletions = false,
-				},
-			},
-		},
-	},
-	html = {
-		capabilities = capabilities,
-	},
-	cssls = {
-		capabilities = capabilities,
-    filetypes = { "html", "css", "vue" }
-	},
-	emmet_ls = {
-		filetypes = { "html", "css" },
-		settings = {
-			root_dir = "~",
-		},
-	},
-	tsserver = {
-		root_dir = function(fname)
-			return vim.loop.cwd()
-		end,
-	},
-}
-
-for server, options in pairs(servers) do
-	options["on_attach"] = on_attach
-	lspconfig[server].setup(options)
-end
-
--- UI Customization
--- diagnostic displayed
-vim.diagnostic.config({
-	virtual_text = false,
-	signs = true,
-	underline = true,
-	update_in_insert = false,
-	severity_sort = false,
-})
-
--- diagnostic symbols
-local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
--- You will likely want to reduce updatetime which affects CursorHold
--- note: this setting is global and should be set only once
-vim.o.updatetime = 250
+require("plugins.configs.lsp.server")
+require("plugins.configs.lsp.diagnostic")
