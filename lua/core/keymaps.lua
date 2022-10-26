d154127578959a665bcb7577ed0b594629e464fb
Akmal <akmalthemalong@gmail.com>
2022-10-26 14:23:42 +0800

    delete useless mappings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,9 +8,5 @@ vim.g.maplocalleader = " "
 -- save changes
 map("n", "ss", "<cmd>w!<cr>")
 
--- easy align
-map("v", "ga", "<Plug>(EasyAlign)", {})
-map("n", "ga", "<Plug>(EasyAlign)", {})
-
 -- exit
 map("n", "q", "<cmd>qa<cr>")
