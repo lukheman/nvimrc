dfe93fe4b3caa0dc6839ea32c2d654a24929c600
Akmal <akmalthemalong@gmail.com>
2022-11-01 21:40:28 +0800

    add comment.

---
+++ lua/plugins/configs/comment.lua
@@ -0,0 +1,17 @@
+local ok, comment = pcall(require, "Comment")
+
+if not ok then
+	return
+end
+
+comment.setup({
+	toggler = {
+		line = "m",
+		block = "gc",
+	},
+	mappings = {
+		basic = true,
+		extra = false,
+		extended = false,
+	},
+})
