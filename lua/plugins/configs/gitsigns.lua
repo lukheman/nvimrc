0368fd542cf2d339e5aef649295db6d710782a14
Akmal <akmalthemalong@gmail.com>
2022-11-03 09:54:33 +0800

    change icon

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -4,13 +4,14 @@ if not ok then
 	return
 end
 
+-- ▍│
 gitsigns.setup({
 	signs = {
-		add = { hl = "GitSignsAdd", text = "▍" },
-		change = { hl = "GitSignsChange", text = "▍" },
-		changedelete = { hl = "GitSignsChange", text = "▍" },
-		delete = { hl = "GitSignsDelete", text = "▍" },
-		topdelete = { hl = "GitSignsDelete", text = "▍" },
+		add = { hl = "GitSignsAdd", text = "│" },
+		change = { hl = "GitSignsChange", text = "│" },
+		delete = { hl = "GitSignsDelete", text = "│" },
+		topdelete = { hl = "GitSignsDelete", text = "│" },
+		changedelete = { hl = "GitSignsChange", text = "│" },
 	},
 	update_debounce = 100,
 })
