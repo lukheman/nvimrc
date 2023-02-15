a17fbf7175e380ed48967f947708521d23d9cf35
Akmal <akmalthemalong@gmail.com>
2023-02-15 16:54:54 +0800

    add bootstraping for packer.nvim

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+vim.defer_fn(function()
+  pcall(require, "impatient")
+end, 0)
+
 -- options
 require("core.options")
 
@@ -7,5 +11,16 @@ require("core.autocmds")
 -- keymaps
 require("core.keymaps")
 
--- load plugins
+-- setup packer
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  print("Installing Packer...")
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd "packadd packer.nvim"
+  require "plugins"
+  vim.cmd "PackerSync"
+end
+
+-- -- load plugins
 require("plugins")
