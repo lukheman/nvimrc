54c58ef5fde9b23c658c9b4a61dcf66990305b04
Akmal <akmalthemalong@gmail.com>
2023-02-15 19:52:08 +0800

    update

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,6 +1,6 @@
-local ok, treesitter = pcall(require, "nvim-treesitter.configs")
+local present, treesitter = pcall(require, "nvim-treesitter.configs")
 
-if not ok then
+if not present then
 	return
 end
 
@@ -16,5 +16,6 @@ treesitter.setup({
 	highlight = {
 		enable = true,
 		use_languagetree = true,
+    additional_vim_regex_highlighting = true
 	},
 })
