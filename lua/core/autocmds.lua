f7b74ca7fe3186f0c4796a8701d359b7b6608d2d
Akmal <akmalthemalong@gmail.com>
2023-03-22 21:25:24 +0800

    implementation api-autocmd

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,20 +1,24 @@
-local filetypes = require("utils").filetypes
+local autocmd = vim.api.nvim_create_autocmd
 
--- filetypes({
-filetypes("cpp", { shiftwidth = 4, tabstop = 4, conceallevel = 0 })
-filetypes("html", { shiftwidth = 2, tabstop = 2 })
-filetypes("vue", { shiftwidth = 2, tabstop = 2 })
-filetypes("css", { shiftwidth = 2, tabstop = 2 })
-filetypes("htmldjango", { shiftwidth = 2, tabstop = 2 })
-filetypes("javascript", { shiftwidth = 2, tabstop = 2 })
-filetypes("json", { shiftwidth = 2, tabstop = 2, conceallevel = 0 })
-filetypes("vim", { shiftwidth = 2, tabstop = 2, conceallevel = 0 })
-filetypes("python", { shiftwidth = 4, tabstop = 4 })
-filetypes("lua", { shiftwidth = 2, tabstop = 2, softtabstop = 2 })
-filetypes("php", { shiftwidth = 2, tabstop = 2, softtabstop = 2 })
-filetypes("xml", { shiftwidth = 2, tabstop = 2, softtabstop = 2 })
-filetypes("text", { nonumber = true, norelativenumber = true })
-filetypes("help", { nonumber = true, norelativenumber = true })
+-- Remove whitespace on save
+-- autocmd("BufWritePre", {
+--   pattern = "",
+--   command = ":%s/\\s\\+$//e"
+-- })
 
--- ingat posisi terakhir cursor
-vim.cmd([[ autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif ]])
+-- Don't auto commenting new line
+autocmd("BufEnter", {
+  pattern = "",
+  command = "set fo-=c fo-=r fo-=o"
+})
+
+-- Set indentation to 2 spaces
+-- autocmd("FileType", {
+--   pattern = { "javascript" },
+--   command = "setlocal tabstop=2 shiftwidth=2"
+-- })
+
+-- last cursor location
+autocmd("BufReadPost", {
+  command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]]
+})
