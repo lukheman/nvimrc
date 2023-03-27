8c57a5b1452d5add449da8a9f4cef9fd31a5daa2
Akmal <akmalthemalong@gmail.com>
2023-03-27 22:12:26 +0800

    change: add and replace some plugins and fix lazy load

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,222 +1,286 @@
-require("lazy").setup({
-  -- dependencies
+-- Bootstrap --
+---------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	-- Dependencies --
+	------------------
 	{ "nvim-lua/plenary.nvim", lazy = true },
 	{ "nvim-lua/popup.nvim", lazy = true },
-	{ "kyazdani42/nvim-web-devicons", lazy = true }, 
+	{ "kyazdani42/nvim-web-devicons", lazy = true },
+	{ "tweekmonster/startuptime.vim", cmd = "StartupTime" },
 
-  -- UI --
-  -- colorschema
-	{
-    "navarasu/onedark.nvim",
-    config = function()
-      require "plugins.configs.onedark"
-    end
-  },
-
-  -- statusline
-  {
-    "rebelot/heirline.nvim",
-    event = "UiEnter",
-    config = function()
-      require "plugins.configs.heirline"
-    end
-  },
-
-  -- bufferline
-  {
-    "willothy/nvim-cokeline",
-    event = "BufRead",
-		config = function()
-      require "plugins.configs.cokeline"
-    end
-  },
-
-  -- indentline
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    cmd = {
-      "IndentBlanklineEnable"
-    },
-		config = function()
-      require "plugins.configs.indentline"
-    end
-  },
-
-  -- file explorer
-	{
-    "kyazdani42/nvim-tree.lua",
-    keys = "<c-n>",
-		config = function()
-      require "plugins.configs.nvimtree"
-    end
-  },
-
-  -- finder file
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = { "<c-f>", "<c-p>" },
-    config = function()
-      require "plugins.configs.telescope"
-    end
-  },
-
-  -- treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = "BufRead",
-    config = function()
-      require "plugins.configs.treesitter"
-    end
-  },
-
-  -- terminal
-	{
-		"akinsho/toggleterm.nvim",
-    keys = "<c-t>",
-		config = function()
-      require "plugins.configs.toggleterm"
-    end 
-	},
-
-  -- Color Picker
-  {
-    "uga-rosa/ccc.nvim",
-    event = "BufRead",
-    config = function()
-      require "plugins.configs.others".ccc()
-    end
-  },
-
-
-  -- EDITOR --
-	-- translator
-	{
-		"voldikss/vim-translator",
-    cmd = "Translate",
-		config = function()
-      require "plugins.configs.others".vim_translator()
-    end
-	},
-
-  -- auto close and auto rename html tag
-  { "windwp/nvim-ts-autotag", ft = { "html", "ejs" }},
-
-  -- comment
-	{
-    "numToStr/Comment.nvim",
-    event = "BufRead",
-		config = require "plugins.configs.comment",
-  },
-
-  -- navigator
-  {
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-    config = function()
-      require("symbols-outline").setup()
-    end
-  },
-
-	-- snippet
-	{
-		"L3MON4D3/LuaSnip", -- snippet engine
-    event = "InsertEnter",
-		dependencies = "rafamadriz/friendly-snippets", -- snippet collection
-		config = function()
-      require "plugins.configs.snippet"
-    end,
-	},
-
-	-- completion
-	{
-		"hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-		dependencies = {
-			"cmp-nvim-lsp",
-			"cmp-buffer",
-			"cmp-path",
-			"cmp-cmdline",
-			"cmp_luasnip",
-		},
-		config = function()
-      require "plugins.configs.cmp"
-    end
-	},
-
-  { "hrsh7th/cmp-nvim-lsp", lazy = true },
-  { "hrsh7th/cmp-buffer", lazy = true },
-  { "hrsh7th/cmp-path", lazy = true },
-  { "hrsh7th/cmp-cmdline", lazy = true },
-  { "saadparwaiz1/cmp_luasnip", lazy = true },
-
-  -- html code completion
-  { "mattn/emmet-vim", ft = "html" },
-
-	-- autopairs
-	{
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-		config = function()
-      require "plugins.configs.autopairs"
-    end
-	},
-
-  -- surround
-  { "machakann/vim-sandwich", event = "InsertEnter"},
-
-	-- autosave
-	{
-		"pocco81/auto-save.nvim",
-    event = "InsertLeave",
-		config = function()
-      require "plugins.configs.autosave"
-    end,
-	},
-
-  -- filetype
-	{
-		"nathom/filetype.nvim",
-		config = function()
-      require "plugins.configs.filetype"
-    end
-	},
-
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    keys = {"<leader>f", mode = { "n", "v" }},
-    config = function()
-      require "plugins.configs.null-ls"
-    end
-  },
 
   -- LSP --
+  ---------
 	{
-    "neovim/nvim-lspconfig",
+		"neovim/nvim-lspconfig",
+		event = { "BufRead", "BufNewFile" },
 		config = function()
-      require "plugins.configs.lsp"
-    end,
+			require("plugins.lsp")
+		end,
 	},
 
-  -- GIT --
-  -- magit for neovim
-  {
-    "TimUntersberger/neogit",
-    cmd = { "Neogit" },
-    config = function()
-      require "plugins.configs.others".neogit()
-    end
-  },
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		-- keys = {"<leader>f", mode = { "n", "v" }},
+		event = { "BufRead", "BufNewFile" },
+		config = function()
+			require("plugins.null-ls")
+		end,
+	},
 
-  -- git integration
+	{
+		"stevearc/aerial.nvim",
+		event = "BufReadPost",
+		cmd = { "AerialToggle", "AerialInfo" },
+		keys = "<leader>at",
+		config = function()
+			require("aerial").setup({
+				backends = {
+					["_"] = { "lsp", "treesitter", "markdown", "man" },
+					markdown = { "treesitter" },
+				},
+				filter_kind = {
+					["_"] = {
+						"Class",
+						"Constructor",
+						"Enum",
+						"EnumMember",
+						"Event",
+						"Field",
+						"Function",
+						"Interface",
+						"Key",
+						"Method",
+						"Module",
+						"Namespace",
+						"Operator",
+						-- "Package", -- this catches for/if ??
+						"Property",
+						"Struct",
+					},
+					-- markdown = { "String" },
+				},
+			})
+			vim.keymap.set("n", "<leader>at", ":AerialToggle<CR>")
+		end,
+	},
+
+	-- {
+	-- 	"simrat39/symbols-outline.nvim",
+	-- 	cmd = "SymbolsOutline",
+	-- 	config = function()
+	-- 		require("symbols-outline").setup()
+	-- 	end,
+	-- },
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = "BufReadPre",
+		cmd = { "TSInstall", "TSUpdate" },
+		config = function()
+			require("plugins.treesitter")
+		end,
+	},
+
+	-- Snippets --
+	--------------
+	{
+		"L3MON4D3/LuaSnip", -- snippet engine
+		event = "InsertEnter",
+		dependencies = "rafamadriz/friendly-snippets", -- snippet collection
+		config = function()
+			require("plugins.snippet")
+		end,
+	},
+
+	{ "mattn/emmet-vim", ft = "html" },
+
+	-- Completion --
+	----------------
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp", lazy = true },
+			{ "hrsh7th/cmp-buffer", lazy = true },
+			{ "hrsh7th/cmp-path", lazy = true },
+			{ "hrsh7th/cmp-cmdline", lazy = true },
+			{ "saadparwaiz1/cmp_luasnip", lazy = true },
+			{ "hrsh7th/cmp-nvim-lsp-signature-help", lazy = true },
+		},
+		config = function()
+			require("plugins.cmp")
+		end,
+	},
+
+
+	-- User Interface --
+	--------------------
+
+	-- Colorschema
+	{
+		"navarasu/onedark.nvim",
+		config = function()
+			require("plugins.onedark")
+		end,
+	},
+
+	-- Statusline
+	{
+		"rebelot/heirline.nvim",
+		event = "UiEnter",
+		config = function()
+			require("plugins.heirline")
+		end,
+	},
+
+	-- Bufferline
+	{
+		"willothy/nvim-cokeline",
+		event = "BufRead",
+		config = function()
+			require("plugins.cokeline")
+		end,
+	},
+
+	-- Indentline
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		cmd = "IndentBlanklineEnable",
+		config = function()
+			require("plugins.indentline")
+		end,
+	},
+
+	-- -- terminal
+	-- {
+	-- 	"akinsho/toggleterm.nvim",
+	-- 	keys = "<c-t>",
+	-- 	config = function()
+	-- 		require("plugins.toggleterm")
+	-- 	end,
+	-- },
+
+	{
+		"rebelot/terminal.nvim",
+		config = function()
+			require("terminal").setup()
+		end,
+	},
+
+	-- Editor Tools --
+	-------------------
+
+	-- File Eexplorer
+	{
+		"kyazdani42/nvim-tree.lua",
+		keys = "<c-n>",
+		config = function()
+			require("plugins.nvimtree")
+		end,
+	},
+
+	-- Finder File
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = { "<c-f>", "<c-p>" },
+		config = function()
+			require("plugins.telescope")
+		end,
+	},
+
+	{
+		"uga-rosa/ccc.nvim",
+		event = "BufRead",
+		config = function()
+			require("plugins.others").ccc()
+		end,
+	},
+
+	{
+		"voldikss/vim-translator",
+		cmd = "Translate",
+		config = function()
+			require("plugins.others").vim_translator()
+		end,
+	},
+
+	{
+		"numToStr/Comment.nvim",
+		event = "BufRead",
+		config = require("plugins.comment"),
+	},
+
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("plugins.autopairs")
+		end,
+	},
+
+	{
+		"kylechui/nvim-surround",
+		keys = {
+			{ mode = "n", "ys" },
+			{ mode = "n", "cs" },
+			{ mode = "n", "ds" },
+			{ mode = "x", "S" },
+		},
+		config = true,
+	},
+
+	{ "windwp/nvim-ts-autotag", ft = { "html", "ejs" } },
+
+	{
+		"pocco81/auto-save.nvim",
+		event = "InsertLeave",
+		config = function()
+			require("plugins.autosave")
+		end,
+	},
+
+	-- GIT --
+	---------
+
+	{
+		"TimUntersberger/neogit",
+		cmd = { "Neogit" },
+		config = function()
+			require("plugins.others").neogit()
+		end,
+	},
+
 	{
 		"lewis6991/gitsigns.nvim",
-    -- cmd = "Gitsigns",
+		event = "BufReadPost",
 		config = function()
-      require "plugins.configs.gitsigns"
-    end
+			require("plugins.gitsigns")
+		end,
 	},
 
-  -- bencmark
-  { "tweekmonster/startuptime.vim", cmd = "StartupTime" },
+	{ "tpope/vim-fugitive", cmd = "G" },
 
-})
+	-- -- filetype
+	-- {
+	-- 	"nathom/filetype.nvim",
+	-- 	config = function()
+	-- 		require("plugins.filetype")
+	-- 	end,
+	-- },
+}
+
+require("lazy").setup(plugins)
