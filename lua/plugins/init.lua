c9c03ff1ae3eca12a07727500b11552d48b762e8
Akmal <akmalthemalong@gmail.com>
2023-03-28 09:15:50 +0800

    change: aerial config file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,35 +47,8 @@ local plugins = {
 		cmd = { "AerialToggle", "AerialInfo" },
 		keys = "<leader>at",
 		config = function()
-			require("aerial").setup({
-				backends = {
-					["_"] = { "lsp", "treesitter", "markdown", "man" },
-					markdown = { "treesitter" },
-				},
-				filter_kind = {
-					["_"] = {
-						"Class",
-						"Constructor",
-						"Enum",
-						"EnumMember",
-						"Event",
-						"Field",
-						"Function",
-						"Interface",
-						"Key",
-						"Method",
-						"Module",
-						"Namespace",
-						"Operator",
-						-- "Package", -- this catches for/if ??
-						"Property",
-						"Struct",
-					},
-					-- markdown = { "String" },
-				},
-			})
-			vim.keymap.set("n", "<leader>at", ":AerialToggle<CR>")
-		end,
+      require "plugins.aerial"
+    end
 	},
 
 	-- {
