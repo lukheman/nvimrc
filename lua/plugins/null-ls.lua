78d969ab1657fcc6ce16ae5f866a7c5d223ebe3a
Akmal <akmalthemalong@gmail.com>
2023-03-28 09:18:16 +0800

    change: null-ls require method

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,20 +1,15 @@
-local ok, nulls = pcall(require, "null-ls")
+local nulls = require("null-ls")
 local map = vim.keymap.set
-
-if not ok then
-	return
-end
 
 nulls.setup({
 	sources = {
 		-- formatting
 		-- lua
 		nulls.builtins.formatting.stylua,
-    -- js, html, css, vue and others
-    nulls.builtins.formatting.prettier
+		-- js, html, css, vue and others
+		nulls.builtins.formatting.prettier,
 	},
 })
-
 
 map("n", "<leader>f", function()
 	vim.lsp.buf.format({ async = true })
