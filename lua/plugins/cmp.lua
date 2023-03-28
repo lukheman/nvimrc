11d138029f8b1a0c8d7ac2a467576ef6ee4c51d9
Akmal <akmalthemalong@gmail.com>
2023-03-28 20:45:37 +0800

    add: decsription

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -120,5 +120,6 @@ cmp.setup.cmdline({ "/", "?" }, {
 	},
 })
 
+-- insert `(` after select function or method item
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
