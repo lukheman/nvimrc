73ec509f241c3a199db5fde09cbf3025625dd8c3
Akmal <akmalthemalong@gmail.com>
2023-03-28 21:39:02 +0800

    change (new): keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,8 +6,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- save changes
-map("n", "<C-w>", "<cmd>w!<cr>")
+map("n", "<leader-w>", "<cmd>w!<cr>")
 map("n", "<leader>wq", "<cmd>wq<cr>")
 
 -- exit
-map("n", "Q", "<cmd>qa<cr>")
+map("n", "q", "<cmd>q!<cr>")
