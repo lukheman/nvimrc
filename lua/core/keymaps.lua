af36b381b0316541c7ce744b78583db7d6cdb3b0
Akmal <akmalthemalong@gmail.com>
2023-05-22 20:03:37 +0800

    add: mappings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,13 +1,12 @@
 local map = vim.keymap.set
 
 -- leader key
--- nnoremap <SPACE> <Nop>
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- save changes
-map("n", "<leader-w>", "<cmd>w!<cr>")
+map("n", "<leader>w", "<cmd>w!<cr>")
 map("n", "<leader>wq", "<cmd>wq<cr>")
 
 -- exit
-map("n", "q", "<cmd>q!<cr>")
+map("n", "qq", "<cmd>q!<cr>")
