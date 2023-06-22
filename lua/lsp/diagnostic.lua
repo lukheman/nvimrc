3dba130fe49905130bdb0f5a5e930231bc4056aa
Akmal <akmalthemalong@gmail.com>
2023-06-22 20:37:27 +0800

    change (new): Diagnostics config

--- lua/lsp/diagnostic.lua
+++ lua/lsp/diagnostic.lua
@@ -1,14 +1,26 @@
-vim.diagnostic.config({
-	virtual_text = false,
+local fn = vim.fn
+local diagnostic = vim.diagnostic
+
+diagnostic.config({
+	-- float = {
+	-- 	source = "always",
+	-- 	border = "rounded",
+	-- 	title = "Diagnostics",
+	-- 	title_pos = "left",
+	-- 	header = "",
+	-- },
+	virtual_text = { prefix = ""},
 	signs = false,
-	underline = false,
+	underline = true,
 	update_in_insert = false,
-	severity_sort = false,
+	severity_sort = true,
 })
 
--- diagnostic symbols
-local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+
+vim.keymap.set("n", "<leader>ld", function()
+	diagnostic.open_float({ scope = "line" })
+end, { desc = "Show line diagnostic" })
