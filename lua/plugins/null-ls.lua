c8a7c58a0743b537f4b0c8ebd19ee76438f2fd35
Akmal <akmalthemalong@gmail.com>
2023-09-19 21:42:55 +0800

    add: eslint format

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -6,16 +6,17 @@ nulls.setup({
 		-- lua
 		nulls.builtins.formatting.stylua,
 		-- js
-    nulls.builtins.diagnostics.eslint.with({
-      prefer_local = eslint_bin
-    }),
-    nulls.builtins.formatting.eslint.with({
-      prefer_local = eslint_bin
-    })
+		nulls.builtins.diagnostics.eslint.with({
+			prefer_local = eslint_bin,
+		}),
+		nulls.builtins.formatting.eslint.with({
+			prefer_local = eslint_bin,
+		}),
 	},
 })
 
 vim.keymap.set("n", "<leader>f", function()
 	vim.lsp.buf.format({ async = true })
-end, { desc = "LSP Format" })
-vim.keymap.set("x", "<leader>f", vim.lsp.buf.format, { desc = "LSP Range Format" })
+end)
+
+vim.keymap.set("x", "<leader>f", vim.lsp.buf.format)
