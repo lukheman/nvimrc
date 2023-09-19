9a51846cad24076ef9d54670588cd58c24549f06
Akmal <akmalthemalong@gmail.com>
2023-09-19 19:42:15 +0800

    change (new config): lspconfig

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,46 +1,30 @@
-local present, lspconfig = pcall(require, "lspconfig")
-if not present then
-	return
-end
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-	silent = true,
-	max_height = "10",
-	border = "single",
-})
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = "single",
-})
-
 -- On Attach --
 ---------------
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
 
-local on_attach = function(client, bufnr)
-	local map = function(mode, key, expr, opts)
-		opts = vim.tbl_extend("keep", { noremap = true, silent = true, buffer = bufnr }, opts)
-		vim.keymap.set(mode, key, expr, opts)
-	end
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	map("n", "gd", require("telescope.builtin").lsp_definitions, { desc = "Go to LSP definition" })
-	map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to LSP definition" })
-	map("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
-	map({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, { desc = "Show LSP Signature Help" })
-	map("n", "<space>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
-  map("n", "gr", require("telescope.builtin").lsp_references, { desc = "Go to LSP references" })
-
-end
-
--- Capabilities --
-------------------
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-local M = {}
-M.on_attach = on_attach
-M.capabilities = capabilities
-return M
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+		-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+		-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		-- vim.keymap.set('n', '<space>wl', function()
+		--   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		-- end, opts)
+		-- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		-- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", "<cmd>Glance references<cr>", opts)
+	end,
+})
