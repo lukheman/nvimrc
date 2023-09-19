0fe39c370bc01358520c0294661643915e1ac6b2
Akmal <akmalthemalong@gmail.com>
2023-09-19 21:45:09 +0800

    add: cmd for LazyLoad

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -212,7 +212,7 @@ local plugins = {
 
 	{
 		"voldikss/vim-translator",
-		cmd = {"Translate", "TranslateR"},
+		cmd = { "Translate", "TranslateR" },
 		config = function()
 			vim.g.translator_target_lang = "id"
 			vim.g.translator_source_lang = "auto"
