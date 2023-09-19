e7c8dc96353a6810a314d840a47761cb91e0e6bc
Akmal <akmalthemalong@gmail.com>
2023-09-19 19:41:13 +0800

    add: buffer action mapping

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,4 +9,8 @@ map("n", "<leader>w", "<cmd>w!<cr>")
 map("n", "<leader>wq", "<cmd>wq<cr>")
 
 -- exit
-map("n", "qq", "<cmd>q!<cr>")
+map("n", "<C-q>", "<cmd>q!<cr>")
+
+map("n", "<leader>p", "<cmd>bp<cr>")
+map("n", "<leader>n", "<cmd>bn<cr>")
+map("n", "<leader>q", "<cmd>bdelete<cr>")
