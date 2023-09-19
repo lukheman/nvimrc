ebfd0429cbe9081a40eebcc5e29841217c258e04
Akmal <akmalthemalong@gmail.com>
2023-09-19 19:33:25 +0800

    change: simplify config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,52 +1,36 @@
 local telescope = require("telescope")
+local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
+local themes = require("telescope.themes")
+local extensions = require("telescope").extensions
 
 telescope.setup({
 	defaults = {
-		vimgrep_arguments = {
-			"rg",
-			"--color=never",
-			"--no-heading",
-			"--with-filename",
-			"--line-number",
-			"--column",
-			"--smart-case",
+		mappings = {
+			i = {
+				["<esc>"] = actions.close,
+			},
 		},
-		-- ❱❱  ❯
-		results_title = false,
-		prompt_title = false,
 		prompt_prefix = "❱❱ ",
 		selection_caret = "❱ ",
-		entry_prefix = "  ",
-		initial_mode = "insert",
-		selection_strategy = "reset",
-		sorting_strategy = "descending",
-		layout_strategy = "horizontal",
+		-- layout_strategy = "center",
 		layout_config = {
-			height = 12,
+			-- 	height = 0.8,
 			width = 0.8,
-			prompt_position = "top",
 		},
-		file_sorter = require("telescope.sorters").get_fuzzy_file,
-		file_ignore_patterns = { "node_modules" },
-		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-		winblend = 0,
-		border = true,
-		borderchars = {
-			prompt = { "─", "│", "─", "│", "╭", "╮", "┤", "├" },
-			results = { " ", "│", "─", "│", "│", "│", "╯", "╰" },
-			preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-		},
-		path_display = { "truncate" },
-		previewer = false,
-		color_devicons = true,
-		use_less = true,
-		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-
-		-- Developer configurations: Not meant for general override
-		buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
 	},
+	pickers = {
+		find_files = {
+			theme = "dropdown",
+		},
+    lsp_references = {
+			theme = "dropdown",
+    },
+		lsp_document_symbols = {
+			theme = "dropdown",
+		},
+	},
+	extensions = {},
 })
-
 vim.keymap.set("n", "<c-f>", builtin.find_files)
 vim.keymap.set("n", "<c-p>", builtin.buffers)
