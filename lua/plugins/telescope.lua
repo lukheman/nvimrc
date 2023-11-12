4328a6e78aa8b2241bd2d49e3271bce9b53757ee
Akmal <akmalthemalong@gmail.com>
2023-11-12 18:41:20 +0800

    change: telescope layout_config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,36 +1,72 @@
 local telescope = require("telescope")
 local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
-local themes = require("telescope.themes")
-local extensions = require("telescope").extensions
 
 telescope.setup({
 	defaults = {
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		prompt_prefix = " ",
+		selection_caret = " ",
+		path_display = { "smart" },
+		scroll_strategy = "limit",
+		selection_strategy = "reset",
+		sorting_strategy = "ascending",
+		layout_strategy = "vertical",
+		layout_config = {
+      width = 0.9,
+			preview_cutoff = 0,
+			vertical = {
+				prompt_position = "bottom",
+				mirror = false,
+			},
+		},
 		mappings = {
 			i = {
 				["<esc>"] = actions.close,
+				["<C-c>"] = actions.close,
+
+				["<CR>"] = actions.select_default,
+				["<C-h>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
+
+				["<C-j>"] = actions.move_selection_next,
+				["<Down>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<Up>"] = actions.move_selection_previous,
+
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
+
+				["<C-?>"] = actions.which_key,
 			},
-		},
-		prompt_prefix = "❱❱ ",
-		selection_caret = "❱ ",
-		-- layout_strategy = "center",
-		layout_config = {
-			-- 	height = 0.8,
-			width = 0.8,
 		},
 	},
 	pickers = {
-		find_files = {
-			theme = "dropdown",
+		live_grep = {
+			vimgrep_arguments = {
+				"rg",
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--smart-case",
+				"--hidden",
+				"--glob=!.git/",
+			},
+			only_sort_text = true,
 		},
-    lsp_references = {
-			theme = "dropdown",
-    },
-		lsp_document_symbols = {
-			theme = "dropdown",
+		find_files = {
+			hidden = false,
+			find_command = { "fd", "--type", "f", "--follow" },
 		},
 	},
 	extensions = {},
 })
+
 vim.keymap.set("n", "<c-f>", builtin.find_files)
 vim.keymap.set("n", "<c-p>", builtin.buffers)
