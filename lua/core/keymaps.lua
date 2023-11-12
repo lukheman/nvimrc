c7015ebc4e7c2aa1a1779a77a773c7c0c4255852
Akmal <akmalthemalong@gmail.com>
2023-11-12 21:39:13 +0800

    nothing important

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,9 +8,10 @@ vim.g.maplocalleader = " "
 map("n", "<leader>w", "<cmd>w!<cr>")
 map("n", "<leader>wq", "<cmd>wq<cr>")
 
--- exit
-map("n", "<C-q>", "<cmd>q!<cr>")
+-- quit
+map("n", "<C-q>", "<cmd>wq!<cr>")
 
+-- buffer
 map("n", "<leader>p", "<cmd>bp<cr>")
 map("n", "<leader>n", "<cmd>bn<cr>")
 map("n", "<leader>q", "<cmd>bdelete<cr>")
