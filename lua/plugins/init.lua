52e570c6f9e3b092cb57555f6ad478e39c3d28d4
Akmal <akmalthemalong@gmail.com>
2023-11-26 14:27:10 +0800

    add: sentiment.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -228,6 +228,19 @@ local plugins = {
 		end,
 	},
 
+	{
+		"utilyre/sentiment.nvim",
+		version = "*",
+		event = "VeryLazy", -- keep for lazy loading
+		opts = {
+			-- config
+		},
+		init = function()
+			-- `matchparen.vim` needs to be disabled manually in case of lazy loading
+			vim.g.loaded_matchparen = 1
+		end,
+	},
+
 	-- surround
 	{
 		"kylechui/nvim-surround",
