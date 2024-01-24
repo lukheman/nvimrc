7567cae95240a2a9b0a366c0f1f4d458c992d161
Akmal <akmalthemalong@gmail.com>
2024-01-24 20:27:48 +0800

    delete useless line

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -323,7 +323,6 @@ local plugins = {
 
 	{
 		"epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
 		lazy = true,
 		ft = "markdown",
 		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
@@ -340,14 +339,6 @@ local plugins = {
 			require("plugins.obsidian")
 		end,
 	},
-
-	-- {
-	-- 	"phaazon/hop.nvim",
-	-- 	config = function()
-	-- 		require("hop").setup()
-	-- 	end,
-	-- },
-
 }
 
 opts = {
