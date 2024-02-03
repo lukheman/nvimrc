8e8b4396232d756a25912f8baa66f31e678760a4
Lukhe <akmalthemalong.com>
2024-02-03 21:15:41 +0800

    add: disable default keys

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,12 @@
 local map = vim.keymap.set
 
+-- disable keys
+map("n", "<Left>", "<Nop>")
+map("n", "<Right>", "<Nop>")
+map("n", "<Up>", "<Nop>")
+map("n", "<Down>", "<Nop>")
+map("n", "<C-z>", "<Nop>")
+
 -- leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -16,5 +23,5 @@ map("n", "<leader>p", "<cmd>bp<cr>")
 map("n", "<leader>n", "<cmd>bn<cr>")
 map("n", "<leader>q", "<cmd>bdelete<cr>")
 
-map('i', 'jj', '<esc>')
-map('n', ':Q', '<cmd>q!<cr>')
+map("i", "jj", "<esc>")
+map("n", ":Q", "<cmd>q!<cr>")
