0233a35468d65778b226a1a9bb27aac67197ae2c
Akmal <akmalthemalong@gmail.com>
2024-05-30 19:41:50 +0800

    penyesuaian keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -12,16 +12,15 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- save changes
-map("n", "<leader>w", "<cmd>w!<cr>")
-map("n", "<leader>wq", "<cmd>wq<cr>")
-
--- quit
--- map("n", "<C-q>", "<cmd>q!<cr>")
+-- map("n", "<leader>w", "<cmd>w!<cr>")
 
 -- buffer
-map("n", "<leader>b", "<cmd>bp<cr>")
+map("n", "<leader>p", "<cmd>bp<cr>")
 map("n", "<leader>n", "<cmd>bn<cr>")
 map("n", "<leader>q", "<cmd>bdelete<cr>")
 
-map("i", "jj", "<esc>")
+-- map("i", "jj", "<esc>")
 map("n", "Q", "<cmd>q!<cr>")
+
+map("n", "<", "<<")
+map("n", ">", ">>")
