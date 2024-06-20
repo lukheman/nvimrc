d73a0165c442fea1c8aed680bc449c0e6497e466
Akmal <akmalthemalong@gmail.com>
2024-06-20 08:07:39 +0800

    disable git signs

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,13 +1,13 @@
 local gitsigns = require("gitsigns")
 
-gitsigns.setup({
-	signs = {
-		add = { hl = "GitSignsAdd", text = "│" },
-		change = { hl = "GitSignsChange", text = "│" },
-		delete = { hl = "GitSignsDelete", text = "│" },
-		topdelete = { hl = "GitSignsDelete", text = "│" },
-		changedelete = { hl = "GitSignsChange", text = "│" },
-		untracked = { hl = "GitSignsUntracked", text = "│" },
-	},
-	signcolumn = true,
-})
+-- gitsigns.setup({
+-- 	signs = {
+-- 		add = { hl = "GitSignsAdd", text = "│" },
+-- 		change = { hl = "GitSignsChange", text = "│" },
+-- 		delete = { hl = "GitSignsDelete", text = "│" },
+-- 		topdelete = { hl = "GitSignsDelete", text = "│" },
+-- 		changedelete = { hl = "GitSignsChange", text = "│" },
+-- 		untracked = { hl = "GitSignsUntracked", text = "│" },
+-- 	},
+-- 	signcolumn = true,
+-- })
