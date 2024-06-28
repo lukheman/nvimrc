3160d01afa0a5d626b9e7127af7e44b811243572
Akmal <akmalthemalong@gmail.com>
2024-06-28 21:39:14 +0800

    new conform config

--- lua/plugins/editor/conform.lua
+++ lua/plugins/editor/conform.lua
@@ -1,26 +1,25 @@
-
 local options = {}
 
 options.config = function()
-  require("conform").setup({
-    formatters_by_ft = {
-      lua = { "stylua" },
-      -- Conform will run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      -- Use a sub-list to run only the first available formatter
-      javascript = { { "prettierd", "prettier" } },
-    },
-    format_on_save = {
-      -- These options will be passed to conform.format()
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-  })
+	require("conform").setup({
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Conform will run multiple formatters sequentially
+			-- python = { "isort", "black" },
+			-- Use a sub-list to run only the first available formatter
+			javascript = { { "prettierd", "prettier" } },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	})
 end
 
 options.spec = {
-  "stevearc/conform.nvim",
-  config = options.config
+	"stevearc/conform.nvim",
+	config = options.config,
 }
 
 return options.spec
