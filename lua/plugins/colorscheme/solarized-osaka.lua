a4c33f20c40e2a72d2f9e32caf06b8d73858300a
Akmal <akmalthemalong@gmail.com>
2024-07-26 06:54:24 +0800

    custom solarized-osaka

--- lua/plugins/colorscheme/solarized-osaka.lua
+++ lua/plugins/colorscheme/solarized-osaka.lua
@@ -4,6 +4,14 @@ return {
 	priority = 1000,
 	opts = {},
 	config = function()
+		require("solarized-osaka").setup({
+			transparent = true,
+			styles = {
+				comments = { italic = true },
+				floats = "normal",
+			},
+		})
+
 		vim.cmd([[colorscheme solarized-osaka]])
 	end,
 }
