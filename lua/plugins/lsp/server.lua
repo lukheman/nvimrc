f50743f6bc355358d15a2b216b7d798ce19899ab
Akmal <akmalthemalong@gmail.com>
2024-09-15 20:52:34 +0800

    add: nvim-navic support

--- lua/plugins/lsp/server.lua
+++ lua/plugins/lsp/server.lua
@@ -1,18 +1,27 @@
 local lspconfig = require("lspconfig")
+local navic = require("nvim-navic")
 
 --Enable (broadcasting) snippet capability for completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+local on_attach = function(client, bufnr)
+	if client.server_capabilities.documentSymbolProvider then
+		navic.attach(client, bufnr)
+	end
+end
 
 local servers = {
 	pyright = {
 		settings = {
 			python = {
 				analysis = {
-					autoImportCompletions = false,
+					autoImportCompletions = true,
 				},
 			},
 		},
+		root_dir = lspconfig.util.root_pattern("*.py"),
+		capabilities = capabilities,
 	},
 	html = {
 		filetypes = { "html", "ejs", "handlebars" },
@@ -37,5 +46,6 @@ local servers = {
 }
 
 for name, config in pairs(servers) do
+	config["on_attach"] = on_attach
 	lspconfig[name].setup(config)
 end
