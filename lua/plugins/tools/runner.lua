478d64d559833a69a1a9c2e93f29fb6b0ef491e3
Akmal <akmalthemalong@gmail.com>
2024-09-28 19:42:16 +0800

    add: c++ and lua handler

--- lua/plugins/tools/runner.lua
+++ lua/plugins/tools/runner.lua
@@ -1,20 +1,22 @@
 local options = {}
 
 function options:config()
-	require("runner").setup({})
+	local runner = require("runner")
 
 	local choice = require("runner.handlers.helpers").choice
 	local helpers = require("runner.handlers.helpers")
+	local command_handler = require("runner.handlers.helpers").command_handler
 
-	require("runner").set_handler(
-		"rust",
-		choice({
-			["Build"] = helpers.shell_handler("cargo build"),
-			["Run"] = helpers.shell_handler("cargo run"),
-			-- ["Test"] = helpers.shell_handler("cargo test"),
-			-- ["Custom"] = helpers.shell_handler("cargo ", true),
-		})
-	)
+	runner.setup({
+		handlers = {
+			rust = choice({
+				["Build"] = helpers.shell_handler("cargo build"),
+				["Run"] = helpers.shell_handler("cargo run"),
+			}),
+			cpp = command_handler("!g++ % -o script.out && ./script.out"),
+			lua = command_handler("luafile %"),
+		},
+	})
 
 	vim.keymap.set("n", "rn", "<cmd>Runner<cr>")
 end
