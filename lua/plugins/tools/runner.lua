637da4b5b1cc6a09a3138217b1e7dd51b8f8f62f
Akmal <akmalthemalong@gmail.com>
2024-10-12 19:58:38 +0800

    refactoring conf

--- lua/plugins/tools/runner.lua
+++ lua/plugins/tools/runner.lua
@@ -1,29 +1,26 @@
-local options = {}
-
-function options:config()
-	local runner = require("runner")
-
-	local choice = require("runner.handlers.helpers").choice
-	local helpers = require("runner.handlers.helpers")
-	local command_handler = require("runner.handlers.helpers").command_handler
-
-	runner.setup({
-		handlers = {
-			rust = choice({
-				["Build"] = helpers.shell_handler("cargo build"),
-				["Run"] = helpers.shell_handler("cargo run"),
-			}),
-			cpp = command_handler("!g++ % -o script.out && ./script.out"),
-			lua = command_handler("luafile %"),
-		},
-	})
-
-	vim.keymap.set("n", "rn", "<cmd>Runner<cr>")
-end
-
-options.spec = {
+return {
 	"MarcHamamji/runner.nvim",
-	config = options.config,
-}
+	enabled = false,
+	config = function()
+		local runner = require("runner")
 
-return options.spec
+		local choice = require("runner.handlers.helpers").choice
+		local helpers = require("runner.handlers.helpers")
+		local command_handler = require("runner.handlers.helpers").command_handler
+
+		runner.setup({
+			position = "bottom",
+			height = 12,
+			handlers = {
+				rust = choice({
+					["Build"] = helpers.shell_handler("cargo build"),
+					["Run"] = helpers.shell_handler("cargo run"),
+				}),
+				cpp = command_handler("!g++ % -o script.out && ./script.out"),
+				lua = command_handler("luafile %"),
+			},
+		})
+
+		vim.keymap.set("n", "rn", "<cmd>Runner<cr>")
+	end,
+}
