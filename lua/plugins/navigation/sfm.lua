6e0380b68cca87f52b6f587dbb69f19be77b9b1e
Akmal <akmalthemalong@gmail.com>
2024-11-27 18:21:23 +0800

    change configuratin structure

--- lua/plugins/navigation/sfm.lua
+++ lua/plugins/navigation/sfm.lua
@@ -1,70 +1,66 @@
-local options = {}
-
-function options:config()
-	require("sfm").setup({
-		view = {
-			side = "left", -- side of the tree, can be `left`, `right`. this setting will be ignored if view.float.enable is set to true,
-			width = 20, -- this setting will be ignored if view.float.enable is set to true,
-			float = {
-				enable = false,
-				config = {
-					relative = "editor",
-					border = "rounded",
-					width = 30, -- int or function
-					height = 30, -- int or function
-					row = 1, -- int or function
-					col = 1, -- int or function
-				},
-			},
-			selection_render_method = "icon", -- render method of selected entries, can be `icon`, `sign`, `highlight`.
-		},
-		mappings = {
-			custom_only = false,
-			list = {
-				-- user mappings go here
-			},
-		},
-		renderer = {
-			icons = {
-				file = {
-					default = "",
-					symlink = "",
-				},
-				folder = {
-					default = "",
-					open = "",
-					symlink = "",
-					symlink_open = "",
-				},
-				indicator = {
-					folder_closed = "", -- 
-					folder_open = "", -- 
-					file = "",
-				},
-				selection = "",
-			},
-		},
-		file_nesting = {
-			enabled = false,
-			expand = false,
-			patterns = {},
-		},
-		misc = {
-			trash_cmd = nil,
-			system_open_cmd = nil,
-		},
-	})
-
-	options.keymap()
-end
-
-function options:keymap()
-	vim.keymap.set("n", "<C-n>", "<cmd>SFMToggle<cr>")
-end
-
-options.spec = {
+return {
 	"dinhhuy258/sfm.nvim",
-	config = options.config,
-}
+	dependencies = {
+		{ "dinhhuy258/sfm-telescope.nvim" },
+	},
+	config = function()
+		local sfm = require("sfm")
+			.setup({
+				view = {
+					side = "left", -- side of the tree, can be `left`, `right`. this setting will be ignored if view.float.enable is set to true,
+					width = 40, -- this setting will be ignored if view.float.enable is set to true,
+					float = {
+						enable = false,
+						config = {
+							relative = "editor",
+							border = "rounded",
+							width = 30, -- int or function
+							height = 30, -- int or function
+							row = 1, -- int or function
+							col = 1, -- int or function
+						},
+					},
+					selection_render_method = "icon", -- render method of selected entries, can be `icon`, `sign`, `highlight`.
+				},
+				mappings = {
+					search = { "/" },
+					custom_only = false,
+					list = {
+						-- user mappings go here
+					},
+				},
+				renderer = {
+					icons = {
+						file = {
+							default = "",
+							symlink = "",
+						},
+						folder = {
+							default = "",
+							open = "",
+							symlink = "",
+							symlink_open = "",
+						},
+						indicator = {
+							folder_closed = "", -- 
+							folder_open = "", -- 
+							file = "",
+						},
+						selection = "",
+					},
+				},
+				file_nesting = {
+					enabled = false,
+					expand = false,
+					patterns = {},
+				},
+				misc = {
+					trash_cmd = nil,
+					system_open_cmd = nil,
+				},
+			})
+			:load_extension("sfm-telescope")
 
-return options.spec
+		vim.keymap.set("n", "<C-n>", "<cmd>SFMToggle<cr>")
+	end,
+}
