0df2d72c6ef5102fcdd47dfd5101c9428c3e3aea
Akmal <akmalthemalong@gmail.com>
2024-11-27 18:18:20 +0800

    fix: type

--- lua/plugins/editor/autosave.lua
+++ lua/plugins/editor/autosave.lua
@@ -3,7 +3,7 @@ function config()
 		trigger_events = { -- See :h events
 			immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
 			defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
-			cancel_defered_save = { "InsertEnter", "ModeChanged" }, -- vim events that cancel a pending deferred save
+			cancel_deferred_save = { "InsertEnter", "ModeChanged" }, -- vim events that cancel a pending deferred save
 		},
 		noautocmd = true,
 		debounce_delay = 100,
