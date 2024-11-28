0052fca3bd3bbcf6160c5305e4755bffdad9222f
Akmal <akmalthemalong@gmail.com>
2024-11-28 11:13:59 +0800

    add event

--- lua/plugins/editor/conform.lua
+++ lua/plugins/editor/conform.lua
@@ -1,25 +1,20 @@
-local options = {}
-
-options.config = function()
-	require("conform").setup({
-		formatters_by_ft = {
-			lua = { "stylua" },
-			-- Conform will run multiple formatters sequentially
-			-- python = { "isort", "black" },
-			-- Use a sub-list to run only the first available formatter
-			javascript = { { "prettierd", "prettier" } },
-		},
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-	})
-end
-
-options.spec = {
+return {
 	"stevearc/conform.nvim",
-	config = options.config,
+	event = "BufWritePre",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				-- python = { "isort", "black" },
+				-- Use a sub-list to run only the first available formatter
+				javascript = { { "prettierd", "prettier" } },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 1000,
+				lsp_fallback = true,
+			},
+		})
+	end,
 }
-
-return options.spec
