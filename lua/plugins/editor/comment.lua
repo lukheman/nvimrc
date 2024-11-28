9b36b6b389320ad7c4a5b055e15a76d9abd23c6e
Akmal <akmalthemalong@gmail.com>
2024-11-28 11:13:51 +0800

    add: keys

--- lua/plugins/editor/comment.lua
+++ lua/plugins/editor/comment.lua
@@ -1,23 +1,20 @@
-local options = {}
-
-options.config = function()
-	local comment = require("Comment")
-
-	comment.setup({
-		toggler = {
-			line = "m",
-			block = "gc",
-		},
-		mappings = {
-			basic = true,
-			extra = false,
-			extended = false,
-		},
-	})
-end
-
 return {
 	"numToStr/Comment.nvim",
-	event = { "BufReadPre", "BufRead", "BufNewFile" },
-	config = options.config,
+	-- event = { "BufReadPre", "BufRead", "BufNewFile" },
+	keys = { "m", "gc" },
+	config = function()
+		local comment = require("Comment")
+
+		comment.setup({
+			toggler = {
+				line = "m",
+				block = "gc",
+			},
+			mappings = {
+				basic = true,
+				extra = false,
+				extended = false,
+			},
+		})
+	end,
 }
