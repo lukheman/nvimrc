789115c4ee396b4c9459024cb8749a191a19e63f
Akmal <akmalthemalong@gmail.com>
2024-11-28 11:14:05 +0800

    change structure

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -1,33 +1,27 @@
-local options = {}
-
-options.config = function()
-	local treesitter = require("nvim-treesitter.configs")
-
-	treesitter.setup({
-		autotag = {
-			enable = true,
-		},
-		ensure_installed = {
-			"lua",
-			"python",
-			"javascript",
-		},
-		highlight = {
-			enable = true,
-			use_languagetree = true,
-			additional_vim_regex_highlighting = true,
-		},
-	})
-end
-
-options.spec = {
+return {
 	"nvim-treesitter/nvim-treesitter",
 	event = "BufReadPre",
 	cmd = { "TSInstall", "TSUpdate" },
-	-- dependencies = {
-	-- 	"windwp/nvim-ts-autotag",
-	-- },
-	config = options.config,
-}
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-return options.spec
+		treesitter.setup({
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"lua",
+				"python",
+				"javascript",
+			},
+			highlight = {
+				enable = true,
+				use_languagetree = true,
+				additional_vim_regex_highlighting = true,
+			},
+		})
+	end,
+}
