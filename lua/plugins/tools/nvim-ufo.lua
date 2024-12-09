a8f79dcc9f9cbba7d0f2bedf7cf05f2c73a34f80
Akmal <akmalthemalong@gmail.com>
2024-12-09 18:40:04 +0800

    update: lazy event

--- lua/plugins/tools/nvim-ufo.lua
+++ lua/plugins/tools/nvim-ufo.lua
@@ -1,7 +1,7 @@
 return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = "kevinhwang91/promise-async",
-	event = "BufReadPre",
+	event = "BufReadPost",
 	config = function()
 		vim.o.foldcolumn = "0" -- '0' is not bad
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
