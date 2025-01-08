f7f44ce92664c4f9aba52ef6fe5fb0bfd9d08e40
Akmal <akmalthemalong@gmail.com>
2025-01-09 07:10:59 +0800

    update: lazy for blink

--- lua/plugins/editor/blink-cmp.lua
+++ lua/plugins/editor/blink-cmp.lua
@@ -1,12 +1,12 @@
 return {
 	"saghen/blink.cmp",
-	lazy = false, -- lazy loading handled internally
 	-- optional: provides snippets for the snippet source
 	dependencies = "rafamadriz/friendly-snippets",
+	event = { "InsertEnter", "CmdLineEnter" },
 
 	-- use a release tag to download pre-built binaries
-	version = "v0.*",
-	-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+	version = "*",
+	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
 	-- build = 'cargo build --release',
 	-- If you use nix, you can build from source using latest nightly rust with:
 	-- build = 'nix run .#build-plugin',
@@ -14,11 +14,12 @@ return {
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
+		signature = { enabled = true },
 		-- 'default' for mappings similar to built-in completion
 		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
 		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-		-- see the "default configuration" section below for full documentation on how to define
-		-- your own keymap.
+		-- See the full "keymap" documentation for information on defining your own keymap.
+
 		keymap = {
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 			["<C-e>"] = { "hide", "fallback" },
@@ -31,39 +32,45 @@ return {
 			["<C-b>"] = { "scroll_documentation_up", "fallback" },
 		},
 
-		highlight = {
-			-- sets the fallback highlight groups to nvim-cmp's highlight groups
-			-- useful for when your theme doesn't support blink.cmp
-			-- will be removed in a future release, assuming themes add support
+		appearance = {
+			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- Useful for when your theme doesn't support blink.cmp
+			-- Will be removed in a future release
 			use_nvim_cmp_as_default = true,
+			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
 		},
-		-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-		-- adjusts spacing to ensure icons are aligned
-		nerd_font_variant = "mono",
 
-		-- experimental auto-brackets support
-		accept = { auto_brackets = { enabled = true } },
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { "lsp", "buffer", "path", "snippets" },
+			cmdline = {},
+			-- cmdline = function()
+			-- 	local type = vim.fn.getcmdtype()
+			-- 	-- Search forward and backward
+			-- 	if type == "/" or type == "?" then
+			-- 		return { "buffer" }
+			-- 	end
+			-- 	-- Commands
+			-- 	if type == ":" or type == "@" then
+			-- 		return { "cmdline" }
+			-- 	end
+			-- 	return {}
+			-- end,
+		},
+		completion = {
 
-		-- experimental signature help support
-		-- trigger = { signature_help = { enabled = true } }
+			ghost_text = { enabled = false },
+			menu = {
+				auto_show = true,
+				-- auto_show = function(ctx)
+				-- 	-- return ctx.mode ~= "cmdline" or not vim.tbl_contains({ "/", "?" }, vim.fn.getcmdtype())
+				-- 	return ctx.mode ~= "cmdline"
+				-- end,
+			},
+		},
 	},
+	opts_extend = { "sources.default" },
 }
-
--- LSP servers and clients communicate what features they support through "capabilities".
---  By default, Neovim support a subset of the LSP specification.
---  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
---  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
---
--- This can vary by config, but in-general for nvim-lspconfig:
-
--- {
---   'neovim/nvim-lspconfig',
---   dependencies = { 'saghen/blink.cmp' },
---   config = function(_, opts)
---     local lspconfig = require('lspconfig')
---     for server, config in pairs(opts.servers or {}) do
---       config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
---       lspconfig[server].setup(config)
---     end
---   end
--- }
