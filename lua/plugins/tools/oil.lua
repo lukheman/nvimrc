e2a55a445e1d7532ce3f11ce7a24585768c6c734
Akmal <akmalthemalong@gmail.com>
2025-01-17 08:27:19 +0800

    updade: lazy load

--- lua/plugins/tools/oil.lua
+++ lua/plugins/tools/oil.lua
@@ -2,6 +2,7 @@ return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
 	---@type oil.SetupOpts
+	cmd = { "Oil" },
 	opts = {},
 	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 	config = function()
